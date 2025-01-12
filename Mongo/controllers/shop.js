d28b09e74b1ed39cbcd62ebead15e998450a0768
Akash <akash.puranik06@gmail.com>
2025-01-12 22:32:36 +0530

    task 6

--- Mongo/controllers/shop.js
+++ Mongo/controllers/shop.js
@@ -53,7 +53,7 @@ exports.getCart = (req, res, next) => {
       });
     })
     .catch((err) => console.log(err));
-    
+
 };
 
 exports.postCart = (req, res, next) => {
@@ -62,13 +62,13 @@ exports.postCart = (req, res, next) => {
     req.user.addToCart(product);
     res.redirect("/cart");
   });
-  
+
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-  .deleteItemFromCart(prodId)
+    .deleteItemFromCart(prodId)
     .then((result) => {
       res.redirect("/cart");
     })
@@ -78,27 +78,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   let fetchedCart;
   req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      return req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch((err) => console.log(err));
-    })
-    .then((result) => {
-      return fetchedCart.setProducts(null);
-    })
+    .addOrder()
     .then((result) => {
       res.redirect("/orders");
     })
@@ -107,7 +87,7 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ["products"] })
+    .getOrders()
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
