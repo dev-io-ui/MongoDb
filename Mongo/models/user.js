878e5a78bcd15a670fa1f695352aa9c8444db0a6
Akash <akash.puranik06@gmail.com>
2025-01-13 18:42:50 +0530

    task 11

--- Mongo/models/user.js
+++ Mongo/models/user.js
@@ -46,6 +46,13 @@ userSchema.methods.addToCart = function (product) {
   return this.save();
 };
 
+userSchema.methods.deleteFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart = updatedCartItems;
+  return this.save();
+};
 
 module.exports = mongoose.model("User", userSchema);
 
